3dbf050e6f946a9bebafceec0f1c0c1e2917f83b
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-10-13 23:29:56 +0500

    Update Table of a given number.py

--- Table of a given number.py
+++ Table of a given number.py
@@ -2,7 +2,8 @@
 
 i=int(input("Enter the number whose table is required: "))
 print("TABLE OF ",i)
-for j in range (1,11,1):    #Loop for multiplication.
-	print(i,"    X","    ",j,"    =","    ",i*j)
+for j in range (1,11):    #Loop for multiplication.
+	#print(i,"    X","    ",j,"    =","    ",i*j)
+	print("{}\tX\t{}\t=\t{}".format(i,j,i*j))
 	
 
